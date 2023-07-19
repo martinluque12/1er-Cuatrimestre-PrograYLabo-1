60f1d9924d0209b221088409147713be83bb45b0
martinluque12 <m.ezequiel.luque@gmail.com>
2023-07-19 11:44:16 -0300

    Add exercise five

---
+++ Clase_3/Ejercicio_05.py
@@ -0,0 +1,28 @@
+'''
+1G MARTIN LUQUE
+
+Ejercicio 05
+
+Pedir al usuario que ingrese 5 números enteros, mostrar los valores ingresados y calcular la suma de los mismos.
+'''
+sum_numbers = 0
+
+separator = '\n*--------------------------------------------*'
+
+for i in range(5):
+    while True:
+        try:
+            entered_number = int(input('\n\nIngrese un numero entero > ').strip())
+            while entered_number < 0:
+                entered_number = int(input('\n¡Error! Debe ingresar solo 5 números enteros, vuelva a intentarlo > ').strip())
+            break
+        except ValueError:
+            print('\n¡Error! Debe ingresar números enteros.')
+    
+    sum_numbers += entered_number
+
+print(separator)
+
+print(f'\nLa suma de los números ingresados es {sum_numbers}')
+
+print(separator)
